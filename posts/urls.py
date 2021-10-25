1481361b0815ca9b5c33bfaacb2348ba1e41359e
Israel Abraham <israelvictory87@gmail.com>
2021-10-25 21:56:19 +0100

    Refactored base and index template

--- posts/urls.py
+++ posts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from posts import views
 
+app_name = "posts"
+
 urlpatterns = [
-    path("", views.home_page, name="home"),
+    path("", views.home_page, name="home_page"),
     path("single-post/", views.single_post_page, name="single_post"),
-    path("create-post/", views.create_post_page, name="create-post")
+    path("create-post/", views.create_post_page, name="create_post")
 ]
