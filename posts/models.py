a695088b9a97e3e16ccc57d9692dcf36da062ab0
Israel Abraham <israelvictory87@gmail.com>
2021-10-26 00:54:53 +0100

    Add image field on Post model

--- posts/models.py
+++ posts/models.py
@@ -4,6 +4,7 @@ from ckeditor.fields import RichTextField
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
+    image = models.ImageField(upload_to="posts_images/", null=True, blank=True)
     content = RichTextField(max_length=200)
     date_created = models.DateTimeField(auto_now_add=True, null=True, blank=True)
 
