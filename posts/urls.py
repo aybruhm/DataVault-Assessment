9e3b9fb1c37c0003b0d14973d2357618fcf8533d
Israel Abraham <israelvictory87@gmail.com>
2021-10-30 22:55:01 +0100

    Created URL paths for register and login

--- posts/urls.py
+++ posts/urls.py
@@ -6,5 +6,9 @@ app_name = "posts"
 urlpatterns = [
     path("", views.home_page, name="home_page"),
     path("single-post/<str:id>/", views.single_post_page, name="single_post"),
-    path("create-post/", views.create_post_page, name="create_post")
+    path("create-post/", views.create_post_page, name="create_post"),
+
+    # Auth URLs
+    path("register/", views.register_user, name="register"),
+    path("login/", views.login_user, name="login")
 ]
