3330dd1bc7acb0d38d0364124ee8fdcb8137ee8d
Israel Abraham <israelvictory87@gmail.com>
2021-10-31 20:36:17 +0100

    Update Post model

--- posts/models.py
+++ posts/models.py
@@ -2,12 +2,13 @@ from django.db import models
 from ckeditor.fields import RichTextField
 from django.contrib.auth.models import User
 
+
 class Post(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to="posts_images/", null=True, blank=True)
     content = RichTextField(max_length=200)
-    date_created = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    date_created = models.DateTimeField(
+        auto_now_add=True, null=True, blank=True)
 
     def __str__(self):
         return self.title
